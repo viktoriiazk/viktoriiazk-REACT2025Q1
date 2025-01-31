9aa2de2f048f7ef85ee5eafa12a94ccb8413626f
Viktoriia Zakorchemna <viktoriia.zakorchemna@gmail.com>
2025-01-31 17:44:38 +0100

    feat: add basic page layout

---
+++ rs-react-app/src/components/Results.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Results extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Results Section</h2>
+        <p>Data will be displayed here.</p>
+      </div>
+    );
+  }
+}
+
+export default Results;
---
+++ rs-react-app/src/components/TopControls.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TopControls extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to the React Components Class Task</h1>
+      </div>
+    );
+  }
+}
+
+export default TopControls;

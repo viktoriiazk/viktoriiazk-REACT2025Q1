5c153230a63c02f7ae79bf46014879d11c3d7ebc
Viktoriia Zakorchemna <viktoriia.zakorchemna@gmail.com>
2025-01-31 18:55:00 +0100

    feat: add search input

--- rs-react-app/src/components/TopControls.tsx
+++ rs-react-app/src/components/TopControls.tsx
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
 
-class TopControls extends Component {
+interface TopControlsProps {
+  searchTerm: string;
+}
+class TopControls extends Component<{}, TopControlsProps> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      searchTerm: '',
+    };
+  }
+
+  handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ searchTerm: event.target.value });
+    console.log(this.state.searchTerm);
+  };
+
   render() {
     return (
       <div>
         <h1>Welcome to the React Components Class Task</h1>
+
+        <div>
+          <input
+            type="text"
+            placeholder="Enter search term..."
+            value={this.state.searchTerm}
+            onChange={this.handleInputChange}
+          />
+          <button>Search</button>
+        </div>
       </div>
     );
   }

f8916b688905ed6f7474674f12906d529171155f
Viktoriia Zakorchemna <viktoriia.zakorchemna@gmail.com>
2025-02-06 16:58:34 +0100

    refactor: update src folder architecture

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import ErrorBoundary from './components/ErrorBoundary.tsx';
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary.tsx';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {

30332bde251d0d598e4f2c9aa3fbda56c2738d7c
Viktoriia Zakorchemna <viktoriia.zakorchemna@gmail.com>
2025-02-08 20:26:36 +0100

    feat: add 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
-
+import Page404 from './pages/404';
 const App = () => {
   return (
     <Router>
@@ -10,6 +11,7 @@ const App = () => {
           path="/viktoriiazk-REACT2025Q1/search/:page"
           element={<HomePage />}
         />
+        <Route path="*" element={<Page404 />} />
       </Routes>
     </Router>
   );
